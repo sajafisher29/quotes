5c8a87ed894c92c252cedd10c111844715d7a84b
Sharina Stubbs <sharinastubbs@gmail.com>
2019-09-27 07:05:31 -0700

    refactored code and added tests.

--- src/test/java/quotes/QuoteTest.java
+++ src/test/java/quotes/QuoteTest.java
@@ -6,11 +6,21 @@ import static org.junit.Assert.*;
 
 public class QuoteTest {
 
+    @Test
+    public void testToString() {
+        Quote aquote = new Quote("Razzle", "Chicken is the best");
+        assertEquals(
+                "instance variables are expected in the making of this string",
+                "Razzle wrote this amazing quote: Chicken is the best",
+                aquote.toString());
+    }
+
+    // checking functionality of constructor
 //    @Test
-//    public void testToString() {
-//        Quote q = new Quote("Razzle", "Chicken please");
+//    public void testQuoteConstructor() {
 //        assertEquals(
-//                "kibble is not as good as chicken",
-//                "Razzle wrote this amazing quote: Chicken please");
+//                "something",
+//
+//        );
 //    }
 }
